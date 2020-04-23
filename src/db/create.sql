749ebf586576c953e9ab70c4816857b745354b40
auburn <auburnete@gmail.com>
2020-04-23 20:46:12 +1000

    A few things at once:
     - The code that implements the stub driver is working! It doesn't
       fail gracefully yet
    
     - download_url is now in the aux_data table instead of the level
       table. this is primarily because the download link is not
       something which vitals actually returns
    
     - I did some thinking about the discord bot, and I think using a
       combination of lowdb [typicode/lowdb] and some cloud storage
       (e.g. GCS) will probably work fine and be significantly
       cheaper (I get 5GB of GCS for free and it's exceedingly unlikely
       we'll ever go above that) than Firestore (current bot)

--- src/db/create.sql
+++ src/db/create.sql
@@ -20,22 +20,22 @@ comment on schema orchard is
     'The Organised Repository of Charts for Rhythm Doctor';
 
 
--- level table
+-- level table. data comes directly from vitals
 create table orchard.level (
     sha256           character (64)   primary key,
     artist           text             not null,
     song             text             not null,
-    "difficulty"     int              not null default 1, -- Medium
-    seizure_warning  boolean          not null default false,
-    description      text,
-    max_BPM          real             not null default 0, -- could be null if the level doesn't have PlaySong events at all
-    min_BPM          real             not null default 0,
+    "difficulty"     int              not null, -- Medium
+    seizure_warning  boolean          not null,
+    description      text             not null,
+    max_BPM          real             not null, -- 0 if no PlaySong events, etc.
+    min_BPM          real             not null,
     last_updated     timestamp        not null,
     single_player    boolean          not null,
     two_player       boolean          not null,
     image_ipfs       text             not null,
-    icon_ipfs        text,            -- levels don't have to have an icon
-    download_url     text             not null
+    hue              real             not null,
+    icon_ipfs        text             -- levels don't have to have an icon
 );
 
 
@@ -69,16 +69,17 @@ order by seq;
 
 -- auxiliary data (not directly from the rdzip)
 create table orchard.aux_data (
-    sha256              character (64)  references orchard.level(sha256),
+    sha256              character (64)  references orchard.level(sha256)    on delete cascade,
+    download_url        text             not null,
     submission_method   text            not null, -- e.g. 'discord', 'steam_workshop', etc
     submission_info     jsonb,          -- optional submission-specific data inserted by the driver.
     iid                 text            not null, -- a submission method specific id.
 
-    -- The "approval level" of the level. Higher levels imply lower levels.
-    -- -1: not approved (failed the competency test?)
-    -- 0: not approved (i.e. awaiting the competency test)
-    -- 1: approved (passed the competency test)
-    -- 2: starred (a level cannot be starred unless it also passed the competency test)
+    -- The "approval level" of the level. Higher levels imply passing lower levels.
+    -- -1: Level -1 is when a level's been reviewed by someone and they've decided it doesn't pass the competency test.
+    -- 0: Level 0 is when a level is awaiting review.
+    -- 1: Level 1 is when a level's been reviewed by someone and they've decided it passes the competency test.
+    -- 2: Level 2 is "starred", which will be levels that get bonus visual emphasis on the website.
     -- 3??? probably won't exist
     approval            int             not null default 0,
 
@@ -111,6 +112,7 @@ grant select on orchard.booster to web_anon;
 
 
 -- role for access
+drop role if exists edit_anon;
 create role edit_anon nologin;
 grant usage on schema orchard to edit_anon;
 grant all on orchard.level to edit_anon;
--- src/sources/client.js
+++ src/sources/client.js
@@ -3,30 +3,28 @@ const axios = require('axios');
 
 
 /**
- * Generate commands to add a level.
+ * Generate a list of commands which add a level. What's a command? A command is an object with
+ * these keys:
  * 
- * @param {} commands 
+ *  - endpoint: the postgREST endpoint we want to hit
+ *  - data: the data to post to that endpoint.
+ * 
+ * Why does it generate a list of commands? Because even one level will have data in multiple tables:
+ *  - the "level" table stores directly data from vitals;
+ *  - the "aux_data" table stores data about the submission method;
+ *  - the "level_tag" and "level_author" are tables to store tags and authors
+ * 
+ * @param data The data to be sent (typically directly from vitals);
+ * @param downloadURL The download URL for the level. This is either of the form ipfs://<ipfs hash> or http[s]://<path to rdzip>.
+ *                    It's always a direct link.
+ * @param submissionMethod A string of the submission method. This needs to be unique.
+ * @param iid The driver-specific individual id
  */
 const levelCommands = (data, downloadURL, submissionMethod, iid) => {
-    // level
+    // level. direct from data except the "tags" and "authors".
     const levelCommand = {
         endpoint: 'level',
-        data: {
-            sha256: data.sha256,
-            artist: data.artist,
-            song: data.song,
-            difficulty: data.difficulty,
-            seizure_warning: data.seizureWarning,
-            description: data.description,
-            max_bpm: data.maxBPM,
-            min_bpm: data.minBPM,
-            last_updated: data.lastUpdated,
-            single_player: data.singlePlayer,
-            two_player: data.twoPlayer,
-            image_ipfs: data.previewImageIpfsHash,
-            icon_ipfs: data.iconIpfsHash,
-            download_url: downloadURL
-        }
+        data: _.pick(data, _.difference(_.keys(data), ["tags", "authors", "rdzip_ipfs"]))
     };
     // tags
     const tagCommands = _.map(data.tags, (tag, idx) => {
@@ -40,12 +38,12 @@ const levelCommands = (data, downloadURL, submissionMethod, iid) => {
         };
     });
     // authors
-    const authorCommands = _.map(data.author, (author, idx) => {
+    const authorCommands = _.map(data.authors, (author, idx) => {
         return {
             endpoint: 'level_author',
             data: {
                 sha256: data.sha256,
-                tag: tag,
+                author: author,
                 seq: idx
             }
         };
@@ -56,7 +54,8 @@ const levelCommands = (data, downloadURL, submissionMethod, iid) => {
         data: {
             sha256: data.sha256,
             submission_method: submissionMethod,
-            iid: iid
+            iid: iid,
+            download_url: downloadURL
         }
     }
 
@@ -69,18 +68,18 @@ const levelCommands = (data, downloadURL, submissionMethod, iid) => {
 }
 
 /**
- * Turns a list of commands into batched set of commands based on common endpoints. Commands are just objects with these keys:
+ * postgREST allows you to send a list of commands under one endpoint for efficiency.
  * 
- *  - endpoint: the endpoint being sent to
- *  - data: the data being sent
+ * This function batches up commands based on common endpoints.
+ * In addition, it sorts the commands in a specific order to satisfy foreign key constraints.
  * 
- * @param {} commands 
+ * @param {} commands - list of commands
  */
 const consolidate = (commands) => {
-    // we have to create a level first before tags, etc.
+    // we have to create a level first before tags, etc due to foreign key constraints.
     const order = ["level", "aux_data", "level_tag", "level_author", "booster"];
 
-    // Get to a form { [endpoint] : [list of commands] }
+    // Get to the form { [endpoint] : [list of commands] }
     const unsortedMap = _.reduce(commands, (prev, curr) => {
         if (!_.includes(_.keys(prev), curr.endpoint)) {
             prev[curr.endpoint] = [];
@@ -101,9 +100,6 @@ const consolidate = (commands) => {
 /**
  * Upload a batch of POST requests to the postgREST server. Commands are just objects with these keys:
  * 
- *  - endpoint: the endpoint we're sending to
- *  - data: the data we're sending
- * 
  * @param {*} commands 
  */
 const runPostRequests = async (commands) => {
@@ -114,6 +110,9 @@ const runPostRequests = async (commands) => {
         let resp = await axios({
             method: 'post',
             url: `${process.env.POSTGREST_SERVER}/${batch.endpoint}`,
+            headers: {
+                authorization: `Bearer ${process.env.BEARER_TOKEN}`
+            },
             data: batch.data
         });
     }
