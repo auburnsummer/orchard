884d81933ab0cad555e01f4404493deb07d90868
auburn <auburnete@gmail.com>
2020-06-26 21:17:24 +1000

    exploratory work on what might be needed for a gdrive driver

---
+++ scraper/src/sources/drivers/gdrive/README.md
@@ -0,0 +1,8 @@
+# gdrive driver
+
+This is a driver for levels hosted in a Google Drive folder.
+
+The folder must be shared read-only to the public.
+
+It could be a personal folder only for your levels, or it could be a folder
+where you and your group can all add levels to it.
---
+++ scraper/src/sources/drivers/gdrive/index.js
@@ -0,0 +1,62 @@
+
+// A driver is a standardised set of functions which implement getting levels from a source.
+// implement these to add support for a service.
+
+/* eslint-disable */
+module.exports = class {
+	// The driver constructor can take any arbitary number of arguments as required, but...
+	constructor({}) {
+		// ...it has to at least expose these two properties:
+
+		// If true, Orchard will rehost the rdzips on IPFS and use that for the download link.
+		// NOTE: Orchard will _always_ rehost the rdzips regardless. This just decides if the DL link is IPFS or not.
+		this.rehost = true;
+
+		// If rehost is false, this needs to be a path from an object returned by expand() to a direct download URL.
+		this.urlPath = "";
+	}
+
+	// Returns some unique string representation of this driver.
+	serialise() {
+
+	}
+
+	// A function which is called when the driver is loaded.
+	async init() {
+
+	}
+
+	// Return an array of iids. This is some parameter which has both the following properties:
+	//  - An iid maps to exactly one level;
+	//  - If the level changes, the iid also changes.
+
+	// for instance, you can't edit a level in Discord without reuploading it, so the URL can be the IID.
+	// however, you can edit Steam Workshop levels, so for Workshop the IID needs to be a combination of
+	// the last updated date and the level ID.
+	async getIids() {
+
+	}
+
+	// Given an IID, return a full driver-specific object.
+	async expand(iid) {
+
+	}
+
+	// Given an IID, return the rdzip that maps to that IID as an arraybuffer (i.e. download the level)
+	async get(iid) {
+
+	}
+
+	// A function which gets called whenever Orchard adds or removes a level from this source.
+	async onChangeHook() {
+
+	}
+
+	// A function which gets called after the driver is done, including on errors.
+	async cleanup() {
+
+	}
+
+};
+
+/* eslint-enable */
