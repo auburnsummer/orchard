bf347b883f8c9e5a8940398a1e5fcc9a888fcb2f
auburn <auburnete@gmail.com>
2020-07-31 08:53:02 +1000

    working out config is annoying, but this seems to work

--- frontend/preact.config.js
+++ frontend/preact.config.js
@@ -1,37 +1,5 @@
-module.exports = (config, env, helpers, params = defaultParams) => {
+const preactCliPostCSS = require('preact-cli-postcss');
 
-    const purgecss = require('@fullhuman/postcss-purgecss')({
-  
-      // Specify the paths to all of the template files in your project
-  
-      content: ['./src/**/*.jsx'],
-  
-      // Include any special characters you're using in this regular expression
-  
-      defaultExtractor: content => content.match(params.regex) || [],
-  
-    });
-  
-    const postCssLoaders = helpers.getLoadersByName(config, 'postcss-loader');
-  
-    postCssLoaders.forEach(({ loader }) => {
-  
-      const plugins = loader.options.plugins;
-  
-      // Add tailwind css at the top.
-  
-      plugins.unshift(require('tailwindcss'));
-  
-      // Add PurgeCSS only in production.
-  
-      if (env.production) {
-  
-        plugins.push(purgecss);
-  
-      }
-  
-    });
-  
-    return config;
-  
-  };
+export default function (config, env, helpers) {
+	preactCliPostCSS(config, helpers);
+}
