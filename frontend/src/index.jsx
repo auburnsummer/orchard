e21229fd69ffded81c55862844bb77ef8867e66c
auburn <auburnete@gmail.com>
2020-08-02 14:11:50 +1000

    add 404 + 200 pages for surge

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,12 +5,14 @@ import style from "./style.css";
 import Router from "preact-router";
 import Home from "./pages/Home";
 import Levels from "./pages/Levels";
+import NotFound from "./pages/404";
 
 export default function App () {
     return (
       <Router>
         <Home path="/" />
         <Levels path="/levels" />
+        <NotFound default />
       </Router>
     )
 }
