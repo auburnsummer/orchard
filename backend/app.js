6e8efd105b6cc4ff025822cac1ff78497c1a3185
auburn <auburnete@gmail.com>
2020-08-03 18:53:09 +1000

    add CORS + helmet

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const db = require("./middleware/db");
 
@@ -23,6 +25,8 @@ app.use(express.json({limit: "100mb"}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(db);
+app.use(cors());
+app.use(helmet());
 
 app.use("/", indexRouter);
 app.use("/groups", groupsRouter);
