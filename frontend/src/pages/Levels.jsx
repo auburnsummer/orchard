0caafa1335c78acee2124cfe6bb734b6dbdee176
auburn <auburnete@gmail.com>
2020-08-12 22:41:15 +1000

    wait it's all flexbox? always has been

--- frontend/src/pages/Levels.jsx
+++ frontend/src/pages/Levels.jsx
@@ -16,14 +16,19 @@ import {useState, useEffect} from "preact/hooks";
 
 function LoadingScreen() {
     return (
-        <p>Loading...</p>
+        <div class="flex flex-col items-center justify-center">
+            <svg class="w-12 h-12 text-white animate-spin" viewBox="0 0 512 512">
+                <path class="fa-secondary" fill="currentColor" d="M479 365zm-22 6l-28-16a16 16 0 01-7-19A184 184 0 11256 72l17 1h-2c-8-1-15-8-15-16V25a16 16 0 0115-16l-15-1a248 248 0 10223 357c-4 7-15 10-22 6z" opacity=".4"/><path class="fa-primary" fill="currentColor" d="M271 73c-8-1-15-8-15-16V25c0-9 8-17 17-16 129 8 231 116 231 247a246 246 0 01-25 108c-4 8-14 11-22 7l-28-16c-8-4-10-14-6-21a183 183 0 0017-78c0-96-74-176-169-183z"/>
+            </svg>
+            <p class="pl-2 mt-6 text-lg font-semibold tracking-wide text-white lowercase">Loading...</p>
+        </div>
     )
 }
 
 function LevelList({levels, _class, state}) {
     const [selectedIndex, setSelectedIndex] = state;
     return (
-        <div class={cm("flex flex-col justify-center p-8 bg-gray-700 bg-opacity-50", _class)} >
+        <div class={cm("flex flex-col justify-center", _class)} >
             {levels.map((level, idx) => {
                 const _class = idx > 0 ? "mt-8" : "";
                 return <LevelHorizontal level={level} selected={selectedIndex === idx} _class={_class} callback={trap(() => setSelectedIndex(prev => idx))} />
@@ -54,9 +59,7 @@ export default function Levels ({p, no}) {
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
     useEffect(() => {
-        console.log("it changed!");
-        console.log(p);
-        console.log(no);
+        setSelectedIndex(-1);
     }, [p, no]);
 
     const style = {
@@ -64,24 +67,24 @@ export default function Levels ({p, no}) {
     }
     
     return (
-        <main class="mx-auto bg-fixed bg-cover" style={style}>
+        <main class="flex flex-col mx-auto bg-fixed bg-cover" style={style}>
             <div class="fixed top-0 z-50 w-full h-16 bg-blue-300">
                 Header {selectedIndex} page {p}
                 <Link href={paramsLink('/levels', {p: parseInt(page) + 1}, defaults)}>Click me!!</Link>
             </div>
-            <div class="flex flex-row items-start justify-center mt-16" onMouseDown={trap(() => setSelectedIndex(prev => -1))}>
+            <div class="flex flex-row items-start justify-center flex-grow mt-16" onMouseDown={trap(() => setSelectedIndex(prev => -1))}>
                 
-                <div class="w-3/5 max-w-3xl">
-                <Switch args={[state]}>
-                    <LoadingScreen test={L(it === "LOADING")} />
+                <div class="flex flex-col justify-center w-3/5 max-w-3xl min-h-screen p-8 pt-24 -mt-16 bg-gray-700 bg-opacity-50">
+                    <Switch args={[state]}>
+                        <LoadingScreen test={L(it === "LOADING")} />
 
-                    <LevelList test={L(it === "LOADED")}
-                        levels={levels}
-                        state={[selectedIndex, setSelectedIndex]}
-                    />
-                                    
-                    <ErrorScreen test={L(it === "ERROR")} error={error}/>
-                </Switch>
+                        <LevelList test={L(it === "LOADED")}
+                            levels={levels}
+                            state={[selectedIndex, setSelectedIndex]}
+                        />
+                                        
+                        <ErrorScreen test={L(it === "ERROR")} error={error}/>
+                    </Switch>
                 </div>
                 
                 <div class="sticky top-0 w-2/5 -mt-16">
