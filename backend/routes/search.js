15258e05ed3101ec93988ea0010dc5cbcc5c6442
auburn <auburnete@gmail.com>
2020-08-21 13:19:07 +1000

    search now uses POST endpoint and updating meili
     - this allows placeholder search!!!!!!!
     - placeholder search!!!!!!!
     - basically, this commit allows search to do placeholder search!!!!!
     - placeholder search!!!!!!
     - woo!!

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -3,35 +3,23 @@ const router = express.Router();
 
 const search = require("../lib/search/search");
 const _ = require("lodash");
-const schema = require("../middleware/schema");
 
-router.get(
+router.post(
 	"/",
-	schema({
-		query: {
-			type: "object",
-			properties: {
-				q: {
-					type: "string"
-				}
-			},
-			required: ["q"]
-		}
-	}),
 	async (req, res, next) => {
 		try {
-			const {knex, query} = req;
+			const {knex, body} = req;
 			// search params we pass to meili
 			const searchParams = [
 				"offset", "limit", "filters", "facetFilters"
 			];
 			const searchQueries = _.reduce(searchParams, (prev, curr) => {
-				if (_.has(query, curr)) {
-					prev[curr] = query[curr];
+				if (_.has(body, curr)) {
+					prev[curr] = body[curr];
 				}
 				return prev;
 			}, {});
-			const result = await search.doSearch(knex, query.q, searchQueries);
+			const result = await search.doSearch(knex, body.q, searchQueries);
 			return res.status(200).json(result);
 		} catch (err) {
 			next(err);
