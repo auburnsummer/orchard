311cea7f2779e26337f530fd1b3b6a99259857cc
auburn <auburnete@gmail.com>
2020-08-25 21:57:18 +1000

    okay here we go
    I watched a video tutorial on elasticsearch. we've got elasticsearch and kibana
    set up in the compose file. next we try out some queries.
    
    in particular I need to make sure it works propertly with CJK, which might involve
    playing around with tokenizers

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -7,23 +7,7 @@ const _ = require("lodash");
 router.post(
 	"/",
 	async (req, res, next) => {
-		try {
-			const {knex, body} = req;
-			// search params we pass to meili
-			const searchParams = [
-				"offset", "limit", "filters", "facetFilters"
-			];
-			const searchQueries = _.reduce(searchParams, (prev, curr) => {
-				if (_.has(body, curr)) {
-					prev[curr] = body[curr];
-				}
-				return prev;
-			}, {});
-			const result = await search.doSearch(knex, body.q, searchQueries);
-			return res.status(200).json(result);
-		} catch (err) {
-			next(err);
-		}
+		return res.status(200).json("hello!");
 	}
 );
 
