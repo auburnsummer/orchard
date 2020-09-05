a6eab69efea28c278aa97260291f6dcd74882eab
auburn <auburnete@gmail.com>
2020-09-06 09:04:18 +1000

    i did this by accident but it looks kinda cool

--- frontend/src/components/levels/LevelBox.jsx
+++ frontend/src/components/levels/LevelBox.jsx
@@ -1,15 +1,31 @@
 import LevelDetail from "./LevelDetail";
 
+import {useState, useEffect} from "preact/hooks";
+
+import cm from "classnames";
+
 
 export default function LevelBox({level, globalSettings}) {
 
+    const [tab, setTab] = useState("level");
+
+    // reset tab to level on change.
+    useEffect(() => {
+        setTab("level");
+    }, [level]);
+
     return (
     <div class="relative flex flex-col-reverse">
-        <LevelDetail _class="p-8 bg-gray-300"level={level} useIPFSLink={globalSettings.useIPFSLinks} showAutoimporter={globalSettings.showAutoimporter}/>
+        <div class="relative">
+            <LevelDetail _class={cm("absolute top-0 left-0 w-full h-full p-8 bg-gray-300 transition-transform duration-500 transform scale-x-0", {"scale-x-100" : tab === "level"})} level={level} useIPFSLink={globalSettings.useIPFSLinks} showAutoimporter={globalSettings.showAutoimporter}/>
+            <div class={cm("absolute top-0 left-0 w-full h-full bg-gray-300 transition-transform duration-500 transform scale-x-0", {"scale-x-100": tab === "group"})}>
+                <p>Group info!</p>
+            </div>
+        </div>
         <div class="flex flex-row text-sm font-thin text-gray-800">
             <div class="flex-grow"></div>
-            <button class="px-2 text-left bg-gray-300">Level</button>
-            <button class="px-2 text-left bg-gray-400 hover:bg-gray-300 hover:cursor-pointer">Group</button>
+            <button onClick={() => setTab("level")} class="px-2 text-left bg-gray-300">Level</button>
+            <button onClick={() => setTab("group")} class="px-2 text-left bg-gray-400 hover:bg-gray-300 hover:cursor-pointer">Group</button>
             <button class="px-2 text-left bg-gray-400 hover:bg-gray-300 hover:cursor-pointer">Comments</button>
         </div>
     </div>
