8d7d7dc9b242bd96020ee169d6440c4691f7350c
1ug1a <elpy.perez@gmail.com>
2020-10-03 11:34:47 -0700

    i just wanted to add ebooks
    i dont really care about hacktoberfest i just wanted to add ebooks quotes lol

--- frontend/src/hooks/useEbooks.js
+++ frontend/src/hooks/useEbooks.js
@@ -21,7 +21,21 @@ const EBOOKS = [
     "DNH: just use -- snap\nDNH: dont be a coward",
     "Bot: Listen I'm busy watching someone slow dance with a tray of enchiladas",
     "okamii: i needed four different pitches of beep so i made 24 instead",
-    "roninnozlo: horny anthem\nSome J Name: what>\nSome J Name: no\nSome J Name: i have never charted a horny song in my life"
+    "roninnozlo: horny anthem\nSome J Name: what>\nSome J Name: no\nSome J Name: i have never charted a horny song in my life",
+    "Rikri: remembe r\nRikri: don't text while defibrillating",
+    "MarioMak967: you all are about as competent as the RHS",
+    "Sealskin: fizzd announces Rhythm Doctor 2\nSealskin: \"The first one isn't even done.\" - KyleLab",
+    "Kin: ah, the 🅱️iolin",
+    "Szprycha: I deleted fizzd",
+    "Fritzy: wait there are other video games",
+    "notDonte: screw kpop in this discord we stan xeno",
+    "plastermaster: chat is going so fast nobody will realize I love you guys",
+    "Hex4Nova: tfw no 190m tall gf\nSome_J_Name: ... 190 meters?",
+    "MarioMak967: good morning everyone!\nMarioMak967: Mak's fun fact of the day: Burying a body vertically makes it a lot harder to find.",
+    "PerefL: We are Becky.\nPerefL: We are many.",
+    "DNH: Let Me Tell You About Blipblop.io",
+    "AdipemDragon: time to bleed ironically",
+    "lugi: lucia has been trapped in the RDCube™️\nlugi: she must atone"
 ];
 
 export default function useEbooks() {
