5a4fac48944eaf17bf6841bff7615e61c256ac4c
auburn <auburnete@gmail.com>
2020-11-14 13:23:15 +1000

    i made some sort of circle thing

--- frontend/src/components/boosters/molecules/BoosterCanvas/BoosterCanvas.jsx
+++ frontend/src/components/boosters/molecules/BoosterCanvas/BoosterCanvas.jsx
@@ -5,19 +5,32 @@ export default function Canvas(props) {
 
     const canvasRef = useRef(null);
 
-    const draw = ctx => {
-        ctx.fillStyle = '#000000'
-        ctx.beginPath()
-        ctx.arc(50, 100, 20, 0, 2*Math.PI)
+    const draw = (ctx, frameCount) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = '#000000';
+        ctx.beginPath();
+        ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
         ctx.fill()
     }
 
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
-        //Our first draw
-        draw(context);
-      }, [])
+        let frameCount = 0;
+        let animationFrameId;
+
+        const render = () => {
+            frameCount++;
+            draw(context, frameCount);
+            animationFrameId = window.requestAnimationFrame(render);
+        }
+
+        render();
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameId);
+        }
+      }, [draw])
 
     return (
         <canvas ref={canvasRef} {...props}>
